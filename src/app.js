e907dcef9dc20bf9aed1454da2baeb0e16160ebb
khotsevich <xozewitc@yandex.ru>
2021-06-27 14:44:32 +0300

    add morgan for logs

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 const express = require('express')
+const morgan = require('morgan')
 
 const app = express()
+
+app.use(morgan('dev'))
 
 app.use('/auth', require('./routes/auth'))
 app.use('/users', require('./routes/users'))
