033eb98b4494c473a4f707493327453ea6d6a380
khotsevich <xozewitc@yandex.ru>
2021-06-28 16:04:45 +0300

    update ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   apps: [
     {
-      name: 'my-spotify-server',
+      name: 'my-spotify',
       script: 'src/index.js',
       env_production: {
         NODE_ENV: 'production'
@@ -13,11 +13,11 @@ module.exports = {
   ],
   deploy: {
     production: {
-      user: 'std',
-      host: 'std-927.ist.mospolytech.ru',
+      user: 'root',
+      host: '89.108.102.195',
       ref: 'origin/master',
       repo: 'git@github.com:mkhotsevich/my-spotify-server.git',
-      path: '/home/std/my-spotify-server',
+      path: '/home/std/mkhotsevich.ru',
       'post-deploy':
         'npm install && pm2 reload ecosystem.config.js --env production'
     }
